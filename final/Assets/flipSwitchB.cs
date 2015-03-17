46f5f03d5bde76b4b6c7cad46a3514918ebce6c0
Vu Dinh <vuniverse143@gmail.com>
2015-03-16 23:31:44 -0700

    Fixed ceiling angle, re-organized scene, implemented two scripts to rotate the light switch on N and M keydowns.

---
+++ final/Assets/flipSwitchB.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipSwitchB : MonoBehaviour {
+	
+	public bool isOn;	// current state
+	
+	// Use this for initialization
+	void Start () {
+		isOn = true;	// room default to light
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.M)) {
+			flip ();
+			
+		}
+	}
+	
+	void OnMouseDown() {
+		flip ();
+	}
+	
+	public void flip() {
+		isOn = !isOn;	// change internal state
+		
+		// change the orientation of the switch
+		float xRotate = 45;
+		if (isOn) {
+			xRotate = -45;
+		}
+		this.transform.localEulerAngles = new Vector3(xRotate, 270, 90);
+		
+		// TODO: change the light states
+		// James already did independently
+	}
+}
