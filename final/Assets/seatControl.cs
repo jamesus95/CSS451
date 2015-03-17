b1264c298f17528a2ad29a9f19c74ab4d0bd0d49
Vu Dinh <vuniverse143@gmail.com>
2015-03-17 13:56:33 -0700

    Finished animation for chair seat. NO MOUSE interaction yet. Conflict with Arm-table as well

---
+++ final/Assets/seatControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class seatControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Animator ani = this.GetComponent<Animator> ();
+		ani.SetBool ("inUse", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.J)) {
+			rotateSeat ();
+		}
+	}
+
+	void onMouseUp () {
+		Debug.Log ("Clicked on chair seat");
+		rotateSeat ();
+	}
+
+	void rotateSeat() {
+		Animator ani = this.GetComponent<Animator> ();
+		ani.SetBool ("inUse", !ani.GetBool ("inUse"));
+		ani.SetTrigger ("Triggered");
+	}
+}
