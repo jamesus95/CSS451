589a27ca09fa7d3415fa81f410f113c47aeab94f
James Murphree <jamesus95@gmail.com>
2015-03-16 19:16:44 -0700

    Light Control Script started

---
+++ CSS 451 workspace/Assets/Scripts/LightControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightControl : MonoBehaviour {
+
+    ArrayList boxLightClose = new ArrayList();
+    ArrayList boxLightFar = new ArrayList();
+    ArrayList smallLights = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("boxLightClose");
+        foreach (GameObject o in temp) {
+            boxLightClose.AddRange(o.GetComponentsInChildren<Light>());
+        }
+        temp = GameObject.FindGameObjectsWithTag("boxLightFar");
+        foreach (GameObject o in temp)
+        {
+            boxLightFar.AddRange(o.GetComponentsInChildren<Light>());
+        }
+        temp = GameObject.FindGameObjectsWithTag("smallLights");
+        foreach (GameObject o in temp)
+        {
+            smallLights.AddRange(o.GetComponentsInChildren<Light>());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SwitchClose(bool on)
+    {
+        foreach (Light l in boxLightClose)
+        {
+            l.enabled = on;
+        }
+    }
+
+    void SwitchFar(bool on)
+    {
+        foreach (Light l in boxLightFar)
+        {
+            l.enabled = on;
+        }
+    }
+
+    void SwitchSmall(bool on)
+    {
+        foreach (Light l in smallLights)
+        {
+            l.enabled = on;
+        }
+    }
+}
