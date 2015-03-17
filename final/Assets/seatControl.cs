42a6cc8f04f548c316f98010ac0ff0c35b1e0422
James Murphree <jamesus95@gmail.com>
2015-03-17 16:53:09 -0700

    presentation

--- final/Assets/seatControl.cs
+++ final/Assets/seatControl.cs
@@ -6,12 +6,12 @@ public class seatControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Animator ani = this.GetComponent<Animator> ();
-		ani.SetBool ("inUse", true);
+		ani.SetBool ("inUse", false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.J)) {
+		if (Input.GetKeyDown(KeyCode.J)) {
 			rotateSeat ();
 		}
 	}
---
+++ final/Assets/tableswivel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tableswivel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.G)) {
+			HingeJoint hj = this.gameObject.GetComponent<HingeJoint>();
+			JointSpring js = hj.spring;
+			js.targetPosition = 90f;
+			hj.spring = js;
+		}
+		if (Input.GetKeyDown(KeyCode.H)) {
+			HingeJoint hj2 = this.gameObject.GetComponent<HingeJoint>();
+			JointSpring js2 = hj2.spring;
+			js2.targetPosition = 0f;
+			hj2.spring = js2;
+		}
+	}
+
+	void OnMouseDown() {
+		HingeJoint hj = this.gameObject.GetComponent<HingeJoint>();
+		JointSpring js = hj.spring;
+		if (js.targetPosition == 0) {
+			js.targetPosition = 90;
+		} else {
+			js.targetPosition = 0;
+		}
+		hj.spring = js;
+	}
+}
